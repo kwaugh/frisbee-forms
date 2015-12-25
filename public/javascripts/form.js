77f117ba648fd7be726401c7b70b78567c7f22f2
Keivaun Waugh <keivaunwaugh@gmail.com>
2015-12-25 07:02:05 +0000

    Adds and removes number input based on quantity

--- public/javascripts/form.js
+++ public/javascripts/form.js
@@ -2,9 +2,15 @@ $(function() {
     var default_number = $('#default-number').val();
     $('.quantity').change(function() {
         var value = $(this).val();    
-        for (var i = 0; i < value; i++) {
-            $(this).parent().after("<div class='form-group col-xs-offset-1'><label>Number:</label><input type='number' class='form-control' name='" + i + "'\
-                value='" + default_number + "' placeholder='Number'></div>");
+        var current_num = $(this).parents('.size').first().children('.number').length;
+        console.log(current_num);
+        console.log('value - current_num =', value - current_num);
+        for (var i = 0; i < value - current_num; i++) { // Add new number boxes
+            $(this).parent().after("<div class='form-group col-xs-offset-1 number'><label>Number:</label><input type='number'\
+                class='form-control' name='" + i + "'value='" + default_number + "' placeholder='Number'></div>");
         }
+        for (var i = 0; i < current_num - value; i++) { // Delete unneeded number boxes
+            $(this).parents('.size:first').children('.number:first').remove();
+        } 
     });
 });
