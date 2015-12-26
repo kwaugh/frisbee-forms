d5260dc92b377d1b3a7957cda7ee8ebe11d739fa
unknown <keivaunwaugh@gmail.com>
2015-12-26 07:39:29 +0000

    Fixed number not being populated bug

--- public/javascripts/form.js
+++ public/javascripts/form.js
@@ -4,10 +4,11 @@ $(function() {
         var value = $(this).val();    
         var current_num = $(this).parents('.size').first().children('.number').length;
         var item_name = $(this).parent().siblings('.item-name:first').text();
+	item_name = item_name.replace("'", "");
         console.log(item_name);
         for (var i = 0; i < value - current_num; i++) { // Add new number boxes
             $(this).parents('.size').append("<div class='form-group col-xs-offset-1 number'><label>Number:</label><input type='number' class=\
-                'form-control' name='number-" + item_name + "-" + (parseInt(i) + parseInt(value)) + "'value='" + default_number + "' placeholder='Number'></div>");
+                'form-control' name='number-" + item_name + "-" + (parseInt(i) + parseInt(value)) + "' value='" + default_number + "' placeholder='Number'></div>");
         }
         for (var i = 0; i < current_num - value; i++) { // Delete unneeded number boxes
             $(this).parents('.size:first').children('.number:first').remove();
