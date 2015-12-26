ba1e886be5eae5f169b103b9b4475177ff159c30
Keivaun Waugh <keivaunwaugh@gmail.com>
2015-12-25 21:12:52 -0600

    Fixed form deletion

--- routes/form_delete.js
+++ routes/form_delete.js
@@ -6,6 +6,7 @@ var forms = DB.collection('forms');
 /* Delete the specified form */
 router.all('/', function(req, res, next) {
     var form = req.body['form-name']; 
+    console.log('deleting form', form);
     if (!form || form === null || form.length === 0) {
         res.redirect('/admin');
         return;
