95e47c06b5145070c094a45ed5972dd9864438bc
unknown <keivaunwaugh@gmail.com>
2015-12-27 04:21:36 +0000

    Fixed security issue where non-admin users can delete forms

--- routes/form_delete.js
+++ routes/form_delete.js
@@ -5,6 +5,10 @@ var forms = DB.collection('forms');
 
 /* Delete the specified form */
 router.all('/', function(req, res, next) {
+    if (!req.session.admin) {
+        res.redirect('/admin');
+        return;
+    }
     var form = req.body['form-name']; 
     console.log('deleting form', form);
     if (!form || form === null || form.length === 0) {
