971a95fcc09584c2fc5193f1b001f18f75082321
Keivaun Waugh <keivaunwaugh@gmail.com>
2016-10-18 23:36:43 +0000

    sort contact_info by team then first name.

--- routes/contact_info.js
+++ routes/contact_info.js
@@ -10,11 +10,24 @@ router.all('/', function(req, res, next) {
     }
     names.find({}, function(err, docs) {
         docs.sort(function(a, b) {
-            if (a.name > b.name)
+            if (a.team == b.team) {
+                // Sort by last name
+                // TODO: Store first and last name separately in DB
+                a_first_name = a.name.substring(0, a.name.indexOf(' '));
+                b_first_name = b.name.substring(0, b.name.indexOf(' '));
+                if (a_first_name > b_first_name) {
+                    return 1;
+                } else if (a_first_name < b_first_name) {
+                    return -1;
+                }
+                return 0;
+            }
+            if (a.team > b.team) {
                 return 1;
-            else if (a.name < b.name)
+            } else if (a.team < b.team) {
                 return -1;
-            return 1;
+            }
+            return 0;
         });
         res.render('contact_info', {'contacts': docs});
     });
