2170cc93d0b875cdfefa563fde590b5d88bf751d
Keivaun Waugh <keivaunwaugh@gmail.com>
2016-10-25 00:13:46 -0400

    only display active forms.

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,12 @@ var forms = DB.collection('forms');
 router.get('/', function(req, res, next) {
     forms.find({}, function(err, doc) {
         console.log('doc:', doc);
+        var current_date = new Date();
+        for (var i in doc) {
+            if (doc[i].date < current_date) {
+                doc.splice(i, 1);
+            }
+        }
         res.render('index', { title: 'Frisbee Order Form' , forms: doc });
     });
 });
