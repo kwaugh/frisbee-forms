a5965d68f395f06e4ff82bcffdf65d6f4816ae24
Keivaun Waugh <keivaunwaugh@gmail.com>
2016-11-01 00:02:28 -0400

    Display open and closed forms separately.

--- routes/index.js
+++ routes/index.js
@@ -6,14 +6,19 @@ var forms = DB.collection('forms');
 /* GET home page. */
 router.get('/', function(req, res, next) {
     forms.find({}, function(err, doc) {
-        console.log('doc:', doc);
+        /* console.log('doc:', doc); */
         var current_date = new Date();
+        var open_forms = [];
+        var closed_forms = [];
         for (var i in doc) {
-            if (doc[i].date < current_date) {
-                doc.splice(i, 1);
+            if (doc[i].date >= current_date) {
+                open_forms.push(doc[i]);
+            } else {
+                closed_forms.push(doc[i]);
             }
         }
-        res.render('index', { title: 'Frisbee Order Form' , forms: doc });
+        res.render('index', { title: 'Frisbee Order Form' , 'open_forms': open_forms,
+            'closed_forms': closed_forms });
     });
 });
 
