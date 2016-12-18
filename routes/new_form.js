5760f80d64f77fab2754efe50102e3d6d6220bcd
Keivaun Waugh <keivaunwaugh@gmail.com>
2016-12-17 20:12:03 -0600

    only update photo_path if a new photo is given.

--- routes/new_form.js
+++ routes/new_form.js
@@ -25,60 +25,66 @@ router.all('/', upload.any(), function(req, res, next) {
         form['_id'] = new ObjectID(req.body['form-id']);
     }
 
-    var item_num_to_id = {};
-    var current_item_num = -1;
-    for (var key in req.body) {
-        if (key.indexOf('item-num') === 0) { // It's a new item
-            var item_id = '';
-            if (hasParam(req, 'id-' + key)) { // updating existing item
-                item_id = new ObjectID(req.body['id-' + key]);
-            } else { // It's a new item
-                item_id = new ObjectID(); // orders are linked to this id
-            }
-            form.items.push({
-                name: req.body[key], sizes: [], subitems: [],
-                'item_id': item_id, photo_path: '', supports_nums: false
-            });
-            item_num_to_id[current_item_num] = item_id;
-            current_item_num++;
-        } else if (key.indexOf('subitem-num') === 0) { // new subitem
-            var subitem_id = '';
-            if (hasParam(req, 'id-' + key)) { // updating existing subitem
-                subitem_id = new ObjectID(req.body['id-' + key]);
-            } else { // it's a new subitem
-                subitem_id = new ObjectID();
-            }
-            form.items[current_item_num].subitems.push(
-                {name: req.body[key], sizes:[], 'subitem_id': subitem_id}
-            );
-        } else if (key.indexOf('item-size') === 0) {
-            form.items[current_item_num].sizes = [].concat(req.body[key]);
-        } else if (key.indexOf('subitem-size') === 0) {
-            form.items[current_item_num].subitems[form.items[current_item_num].
-                subitems.length - 1].sizes = [].concat(req.body[key]); 
-        } else if (key.indexOf('supports-nums') === 0) {
-            form.items[current_item_num].supports_nums = true;
-        } else if (key.indexOf('close-datetime') === 0) { // The closing datetime
-            var server_offset = new Date().getTimezoneOffset();
-            var client_offset = req.body['timezone-offset'];
-            var orig_time = new Date(req.body[key]);
-            orig_time = new Date(orig_time.getTime() + client_offset * 60 * 1000);
-            form.date = new Date(orig_time.getTime() +
-                    ((client_offset - server_offset) * 60 * 1000));
-            form.client_date_offset = client_offset;
-        } else if (key.indexOf('form-live') === 0) { // is the form live?
-            form.live = true;
+    forms.findOne({_id: form['_id']}, function(err, the_form) {
+        var id_to_item = {};
+        for (var item of the_form.items) {
+            id_to_item[item.item_id] = item;
         }
-    }
-    console.log('form:', form);
-    for (var file in req.files) { // these are the picture uploads
-        var item_num = parseInt(file.substring(file.indexOf('-') + 1));
-        var item_id = item_num_to_id[item_num];
-        console.log('item_num:', item_num);
-        form.items[item_num].photo_path = req.files[file].path;
-    }
-    forms.save(form);
-    res.redirect('admin');
+        var item_num_to_id = {};
+        var current_item_num = -1;
+        for (var key in req.body) {
+            if (key.indexOf('item-num') === 0) { // It's a new item
+                var item_id = '';
+                var photo_path = '';
+                if (hasParam(req, 'id-' + key)) { // updating existing item
+                    item_id = new ObjectID(req.body['id-' + key]);
+                    photo_path = id_to_item[item_id].photo_path;
+                } else { // It's a new item
+                    item_id = new ObjectID(); // orders are linked to this id
+                }
+                form.items.push({
+                    name: req.body[key], sizes: [], subitems: [],
+                    'item_id': item_id, 'photo_path': photo_path, supports_nums: false
+                });
+                item_num_to_id[current_item_num] = item_id;
+                current_item_num++;
+            } else if (key.indexOf('subitem-num') === 0) { // new subitem
+                var subitem_id = '';
+                if (hasParam(req, 'id-' + key)) { // updating existing subitem
+                    subitem_id = new ObjectID(req.body['id-' + key]);
+                } else { // it's a new subitem
+                    subitem_id = new ObjectID();
+                }
+                form.items[current_item_num].subitems.push(
+                    {name: req.body[key], sizes:[], 'subitem_id': subitem_id}
+                );
+            } else if (key.indexOf('item-size') === 0) {
+                form.items[current_item_num].sizes = [].concat(req.body[key]);
+            } else if (key.indexOf('subitem-size') === 0) {
+                form.items[current_item_num].subitems[form.items[current_item_num].
+                    subitems.length - 1].sizes = [].concat(req.body[key]);
+            } else if (key.indexOf('supports-nums') === 0) {
+                form.items[current_item_num].supports_nums = true;
+            } else if (key.indexOf('close-datetime') === 0) { // The closing datetime
+                var server_offset = new Date().getTimezoneOffset();
+                var client_offset = req.body['timezone-offset'];
+                var orig_time = new Date(req.body[key]);
+                orig_time = new Date(orig_time.getTime() + client_offset * 60 * 1000);
+                form.date = new Date(orig_time.getTime() +
+                    ((client_offset - server_offset) * 60 * 1000));
+                form.client_date_offset = client_offset;
+            } else if (key.indexOf('form-live') === 0) { // is the form live?
+                form.live = true;
+            }
+        }
+        for (var file in req.files) { // these are the picture uploads
+            var item_num = parseInt(file.substring(file.indexOf('-') + 1));
+            var item_id = item_num_to_id[item_num];
+            form.items[item_num].photo_path = req.files[file].path;
+        }
+        forms.save(form);
+        res.redirect('admin');
+    });
 });
 
 module.exports = router;
